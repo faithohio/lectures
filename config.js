8fde397ca54bc9c9f5de00617ab26ec784314603
faithohio <faithaziegbemi@gmail.com>
2026-05-14 20:41:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 // STEP 1: After setting up Google Apps Script, paste your Web App URL here.
 // Leave empty ("") until you've completed the Google Sheets setup guide.
-var GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbylP7NImRnCS0at6_rbUaDrbwcdOL1BLN-ZQTPKNl0_y5Cv2g9tghL9HfhLPwph6hcOOA/exec";
+var GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxmyvgdAwr1cadu-JqBDZuPBg07TXOuL463TfBhqalGKywjr2qWPIQ-3NswQNMZxmxJWQ/exec";
 
 // STEP 2: Change this to your own admin password.
 // You'll use this to log into admin.html
